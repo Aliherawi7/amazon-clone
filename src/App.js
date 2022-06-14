f345c56464510838b503dab3ab4e0f309bab6a2f
aliherawi7 <aliherawi7@gmail.com>
2022-06-14 09:45:50 +0430

    /signup Route added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './Header'
 import Home from './Home'
 import Checkout from './Checkout';
 import Login from './Login'
+import Signup from './Signup'
 import Payment from './Payment'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path="/checkout" element={<Checkout/>} />
           <Route path="/" element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
           <Route path="/payment" element={<Payment/>} />
           <Route path="*" element={<h1 style={{textAlign:'center',marginTop:'40px'}}>Not found</h1>}/>
         </Routes>
